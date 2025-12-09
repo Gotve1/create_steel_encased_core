e227887b7ae4a00e2e3762a6f1bb595cc5247412
Gotve <akselvolkov40@gmail.com>
2025-12-09 11:39:53 +0500

    Added new tiers and items that use these tiers

--- src/main/java/com/gotve/create_steel_encased_core/item_registry/ItemRegistry.java
+++ src/main/java/com/gotve/create_steel_encased_core/item_registry/ItemRegistry.java
@@ -1,6 +1,6 @@
 package com.gotve.create_steel_encased_core.item_registry;
 
-import com.gotve.create_steel_encased_core.mod_tiers.ModTiers;
+import com.gotve.create_steel_encased_core.mod_tiers.ItemTiers;
 import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,50 +14,99 @@ public class ItemRegistry {
 
 
     /// //////////////////////////
-    ///      Copper Tier      ///
+    ///      Copper Tier       ///
     /// //////////////////////////
     public static final RegistryObject<Item> STONE_AXE = MINECRAFT_ITEMS.register("stone_axe",
-            () -> new AxeItem(ModTiers.COPPER_TIER, 7.0F, -3.2F, new Item.Properties())
+            () -> new AxeItem(ItemTiers.COPPER_TIER, 7.0F, -3.2F, new Item.Properties())
     );
 
     public static final RegistryObject<Item> STONE_SWORD = MINECRAFT_ITEMS.register("stone_sword",
-            () -> new SwordItem(ModTiers.COPPER_TIER, 3, -2.4f, new Item.Properties())
+            () -> new SwordItem(ItemTiers.COPPER_TIER, 3, -2.4f, new Item.Properties())
     );
 
     public static final RegistryObject<Item> STONE_HOE = MINECRAFT_ITEMS.register("stone_hoe",
-            () -> new HoeItem(ModTiers.COPPER_TIER, -1, -2.0F, new Item.Properties())
+            () -> new HoeItem(ItemTiers.COPPER_TIER, -1, -2.0F, new Item.Properties())
     );
 
     public static final RegistryObject<Item> STONE_PICKAXE = MINECRAFT_ITEMS.register("stone_pickaxe",
-            () -> new PickaxeItem(ModTiers.COPPER_TIER, 1, -2.8F, new Item.Properties())
+            () -> new PickaxeItem(ItemTiers.COPPER_TIER, 1, -2.8F, new Item.Properties())
     );
 
     public static final RegistryObject<Item> STONE_SHOVEL = MINECRAFT_ITEMS.register("stone_shovel",
-            () -> new ShovelItem(ModTiers.COPPER_TIER, 1.5F, -3.0F, new Item.Properties())
+            () -> new ShovelItem(ItemTiers.COPPER_TIER, 1.5F, -3.0F, new Item.Properties())
     );
 
 
+
     /// //////////////////////////
-    ///       Brass tier      ///
+    ///       Brass tier       ///
     /// //////////////////////////
     public static final RegistryObject<Item> BRASS_AXE = MINECRAFT_ITEMS.register("iron_axe",
-            () -> new AxeItem(ModTiers.BRASS_TIER, 6.0F, -3.1F, new Item.Properties())
+            () -> new AxeItem(ItemTiers.BRASS_TIER, 6.0F, -3.1F, new Item.Properties())
     );
 
     public static final RegistryObject<Item> BRASS_SWORD = MINECRAFT_ITEMS.register("iron_sword",
-            () -> new SwordItem(ModTiers.BRASS_TIER, 3, -2.4F, new Item.Properties())
+            () -> new SwordItem(ItemTiers.BRASS_TIER, 3, -2.4F, new Item.Properties())
     );
 
     public static final RegistryObject<Item> BRASS_HOE = MINECRAFT_ITEMS.register("iron_hoe",
-            () -> new HoeItem(ModTiers.BRASS_TIER, -2, -1.0F, new Item.Properties())
+            () -> new HoeItem(ItemTiers.BRASS_TIER, -2, -1.0F, new Item.Properties())
     );
 
     public static final RegistryObject<Item> BRASS_PICKAXE = MINECRAFT_ITEMS.register("iron_pickaxe",
-            () -> new PickaxeItem(ModTiers.BRASS_TIER, 1, -2.8F, new Item.Properties())
+            () -> new PickaxeItem(ItemTiers.BRASS_TIER, 1, -2.8F, new Item.Properties())
     );
 
     public static final RegistryObject<Item> BRASS_SHOVEL = MINECRAFT_ITEMS.register("iron_shovel",
-            () -> new ShovelItem(ModTiers.BRASS_TIER, 1.5F, -3.0F, new Item.Properties())
+            () -> new ShovelItem(ItemTiers.BRASS_TIER, 1.5F, -3.0F, new Item.Properties())
+    );
+
+
+    /// //////////////////////////
+    ///       Steel tier       ///
+    /// //////////////////////////
+    public static final RegistryObject<Item> STEEL_AXE = MINECRAFT_ITEMS.register("diamond_axe",
+            () -> new AxeItem(ItemTiers.STEEL_TIER, 5.0F, -3.0F, new Item.Properties())
+    );
+
+    public static final RegistryObject<Item> STEEL_SWORD = MINECRAFT_ITEMS.register("diamond_sword",
+            () -> new SwordItem(ItemTiers.STEEL_TIER, 3, -2.4F, new Item.Properties())
+    );
+
+    public static final RegistryObject<Item> STEEL_HOE = MINECRAFT_ITEMS.register("diamond_hoe",
+            () -> new HoeItem(ItemTiers.STEEL_TIER, -3, 0.0F, new Item.Properties())
+    );
+
+    public static final RegistryObject<Item> STEEL_PICKAXE = MINECRAFT_ITEMS.register("diamond_pickaxe",
+            () -> new PickaxeItem(ItemTiers.STEEL_TIER, 1, -2.8F, new Item.Properties())
+    );
+
+    public static final RegistryObject<Item> STEEL_SHOVEL = MINECRAFT_ITEMS.register("diamond_shovel",
+            () -> new ShovelItem(ItemTiers.STEEL_TIER, 1.5F, -3.0F, new Item.Properties())
+    );
+
+
+    /// //////////////////////////
+    ///     Nethersteel tier   ///
+    /// //////////////////////////
+    public static final RegistryObject<Item> NETHERSTEEL_AXE = MINECRAFT_ITEMS.register("netherite_axe",
+            () -> new AxeItem(ItemTiers.NETHERSTEEL_TIER, 5.0F, -3.0F, new Item.Properties())
+    );
+
+    public static final RegistryObject<Item> NETHERSTEEL_SWORD = MINECRAFT_ITEMS.register("netherite_sword",
+            () -> new SwordItem(ItemTiers.NETHERSTEEL_TIER, 3, -2.4F, new Item.Properties())
+    );
+
+    public static final RegistryObject<Item> NETHERSTEEL_HOE = MINECRAFT_ITEMS.register("netherite_hoe",
+            () -> new HoeItem(ItemTiers.NETHERSTEEL_TIER, -3, 0.0F, new Item.Properties())
+    );
+
+    public static final RegistryObject<Item> NETHERSTEEL_PICKAXE = MINECRAFT_ITEMS.register("netherite_pickaxe",
+            () -> new PickaxeItem(ItemTiers.NETHERSTEEL_TIER, 1, -2.8F, new Item.Properties())
+    );
+
+    public static final RegistryObject<Item> NETHERSTEEL_SHOVEL = MINECRAFT_ITEMS.register("netherite_shovel",
+            () -> new ShovelItem(ItemTiers.NETHERSTEEL_TIER, 1.5F, -3.0F, new Item.Properties())
     );
 
 
---
+++ src/main/java/com/gotve/create_steel_encased_core/mod_tiers/ItemTiers.java
@@ -0,0 +1,54 @@
+package com.gotve.create_steel_encased_core.mod_tiers;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+
+@SuppressWarnings("removal")
+public class ItemTiers {
+
+    public static final Tier COPPER_TIER = new Tier() {
+        @Override public int getUses() { return 131; }
+        @Override public float getSpeed() { return 4.0f; }
+        @Override public float getAttackDamageBonus() { return 1.0f; }
+        @Override public int getLevel() { return 1; }
+        @Override public int getEnchantmentValue() { return 5; }
+        @Override public Ingredient getRepairIngredient() {
+            return Ingredient.of(ItemTags.create(new ResourceLocation("forge", "ingots/copper")));
+        }
+    };
+
+    public static final Tier BRASS_TIER = new Tier() {
+        @Override public int getUses() { return 250; }
+        @Override public float getSpeed() { return 6.0f; }
+        @Override public float getAttackDamageBonus() { return 2.0f; }
+        @Override public int getLevel() { return 2; }
+        @Override public int getEnchantmentValue() { return 10; }
+        @Override public Ingredient getRepairIngredient() {
+            return Ingredient.of(ItemTags.create(new ResourceLocation("forge", "ingots/brass")));
+        }
+    };
+
+    public static final Tier STEEL_TIER = new Tier() {
+        @Override public int getUses() { return 1561; }
+        @Override public float getSpeed() { return 8.0F; }
+        @Override public float getAttackDamageBonus() { return 3.0F; }
+        @Override public int getLevel() { return 3; }
+        @Override public int getEnchantmentValue() { return 10; }
+        @Override public Ingredient getRepairIngredient() {
+            return Ingredient.of(ItemTags.create(new ResourceLocation("forge", "ingots/steel")));
+        }
+    };
+
+    public static final Tier NETHERSTEEL_TIER = new Tier() {
+        @Override public int getUses() { return 2031; }
+        @Override public float getSpeed() { return 9.0F; }
+        @Override public float getAttackDamageBonus() { return 4.0F; }
+        @Override public int getLevel() { return 4; }
+        @Override public int getEnchantmentValue() { return 15; }
+        @Override public Ingredient getRepairIngredient() {
+            return Ingredient.of(ItemTags.create(new ResourceLocation("forge", "ingots/nethersteel")));
+        }
+    };
+}
